4fd799e53ffbd168588a95be37a5df868b944aea
Joshua Olugbemi <fijacks2005@yahoo.com>
2025-05-14 15:09:09 +0100

    Update settings.py

--- shivtirth_project/settings.py
+++ shivtirth_project/settings.py
@@ -176,13 +176,13 @@ DATABASES = {
     }
 }
 
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
 }
-
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 MEDIA_URL = 'media/'
 
