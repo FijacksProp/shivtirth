aa7f65fd6cd6dbaec3787843de7808e7a900c8e5
Joshua Olugbemi <fijacks2005@yahoo.com>
2025-05-16 20:28:09 +0100

    Update settings.py

--- shivtirth_project/settings.py
+++ shivtirth_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hef2$p$q@n^%1v1^%9t_rss*av08tgl%q@eq2oc(lenv=r&-s+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
